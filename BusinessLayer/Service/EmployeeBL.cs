b46c473f939e4d116423565d37d53bcfb08adf84
Harshada Lohar <Harshadal@emids.com>
2022-07-19 22:42:17 -0700

    [Harshada]Added:Add Employee By Admin

---
+++ BusinessLayer/Service/EmployeeBL.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLayer.Interface;
+using DatabaseLayer.Model;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Service
+{
+    public class EmployeeBL:IEmployeeBL
+    {
+        private readonly IEmployeeRL EmployeeRL;
+
+        public EmployeeBL(IEmployeeRL EmployeeRL)
+        {
+            this.EmployeeRL = EmployeeRL;
+        }
+
+        public EmployeeModel AddEmployee(EmployeeModel employee)
+        {
+            try
+            {
+                return this.EmployeeRL.AddEmployee(employee);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+    }
+}
